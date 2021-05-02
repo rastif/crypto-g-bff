1cc0348c4559fd98f19e31919352c5a30e817532
rastif <rasti.faradj@hotmail.com>
2021-05-03 00:31:25 +0200

    ugly hack so we dont exceed lambda limit on body

--- api/v1/assets.ts
+++ api/v1/assets.ts
@@ -10,7 +10,13 @@ axios.defaults.headers.common["X-CoinAPI-Key"] = config.API_KEY;
 const assets = async (req: VercelRequest, res: VercelResponse) => {
   const resp = await axios.get("/v1/assets");
   const normalizedAssets = normalizeAssets(resp.data);
-  res.status(200).json(normalizedAssets);
+
+  // ugly hack so we dont exceed 5mb body size limit
+  // limitation of Vercel Serverless Functions
+  const halfArrLength = Math.ceil(normalizedAssets.length / 2);
+  const resArr = normalizedAssets.splice(0, halfArrLength);
+
+  res.status(200).json(resArr);
 };
 
 export default allowCors(assets);
