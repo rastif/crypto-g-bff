852305e2c78877c61895b6274f2aa52f6232cce6
rastif <rasti.faradj@hotmail.com>
2021-05-03 01:27:36 +0200

    new free api key

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   API_BASE: "https://rest.coinapi.io",
-  API_KEY: "FF352A24-78BF-4D49-B564-73D64CD27BF3",
+  API_KEY: "C745A3A0-05B5-40FB-8244-E022FBA7F8D6",
 };
 
 export default config;
